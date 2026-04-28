e5787c137a2d86d3fd0a4c685bcab946518641ea
anasteisha2525-maker <anasteisha2525@gmail.com>
2026-04-28 16:38:33 +0300

    Update 6.2.c

--- 6.2.c
+++ 6.2.c
@@ -1,51 +1,71 @@
 #include <stdio.h>
 
 int main() {
-    int n, m;
+    int choice;
 
-    printf("Введіть n і m: ");
-    scanf("%d %d", &n, &m);
+    do {
+        printf("\n=== МЕНЮ ===\n");
+        printf("1 - Виконати задачу\n");
+        printf("0 - Вихід\n");
+        printf("Ваш вибір: ");
+        scanf("%d", &choice);
 
-    if (n <= 0 || m <= 0) {
-        printf("Помилка: некоректні розміри\n");
-        return 1;
-    }
+        if (choice == 1) {
+            int n, m;
 
-    int A[100][100];                // задаємо максимум, щоб можна було вводити різні розміри
+            printf("\nВведіть кількість рядків: ");
+            scanf("%d", &n);
+            printf("Введіть кількість стовпців: ");
+            scanf("%d", &m);
 
-    printf("Введіть матрицю:\n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            scanf("%d", &A[i][j]);
-        }
-    }
-       //шукаємо максимальне
-    int max = A[0][0];              
-    int row = 0, col = 0;           // тут будемо зберігати позицію максимуму
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (A[i][j] > max) {
-                max = A[i][j];
-                row = i;
-                col = j;
+            int a[n][m];
+
+            printf("\nВведіть елементи матриці:\n");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    printf("a[%d][%d] = ", i, j);
+                    scanf("%d", &a[i][j]);
+                }
+            }
+            printf("\nПочаткова матриця:\n");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    printf("%4d", a[i][j]);
+                }
+                printf("\n");
+            }
+            int max = a[0][0];          // беремо перший елемент як максимальний
+            int row = 0, col = 0;
+                // Пошук максимального елемента
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (a[i][j] > max) {
+                        max = a[i][j];
+                        row = i;       // змінні для номера рядка і стовпця максимуму
+                        col = j;
+                    }
+                }
+            }
+            printf("\nМаксимальний елемент: %d\n", max);
+            printf("Його позиція: [%d][%d]\n", row, col);
+            // Вивід нової матриці без рядка і стовпця
+            printf("\nНова матриця:\n");
+
+            for (int i = 0; i < n; i++) {
+                if (i == row)
+                    continue;
+
+                for (int j = 0; j < m; j++) {
+                    if (j == col)
+                        continue;
+
+                    printf("%4d", a[i][j]);
+                }
+                printf("\n");
             }
         }
-    }
 
-    printf("Макс = %d (рядок %d, стовпець %d)\n", max, row + 1, col + 1);
-
-    printf("Результат:\n");
-    // вивід матриці без потрібного рядка і стовпця
-    for (int i = 0; i < n; i++) {
-        if (i == row) continue;
-
-        for (int j = 0; j < m; j++) {
-            if (j == col) continue;
-
-            printf("%d ", A[i][j]);
-        }
-        printf("\n");
-    }
+    } while (choice != 0);
 
     return 0;
 }
